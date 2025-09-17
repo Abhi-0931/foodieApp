e77af718034e72d3ab66bedee76140cddaa9d6e0
Abhishek Mulinti <abhishek.m@EIS.LOCAL>
2025-09-17 18:33:26 +0530

    Initial commit

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const mobileMenu = document.querySelector(".mobile-menu");
 hamburger.addEventListener("click", () => {
   mobileMenu.classList.toggle("mobile-menu-active");
 });
+var swiper = new Swiper(".mySwiper", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
